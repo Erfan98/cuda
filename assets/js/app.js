08f8ea3b0f5211231a97bc75d50ee23618902922
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2020-11-05 11:51:49 +0600

    Lots of Changes and Skill Section Added

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+$('.our-skill').circleProgress({
+    size:150,
+    thickness:15
+  }).on('circle-animation-progress', function(event, progress) {
+      var percentvalue=$(this).find('span').attr('data-value')
+    $(this).find('span').html(Math.round(percentvalue * progress) + '<i>%</i>');
+    console.log(progress);
+  });
+ 
