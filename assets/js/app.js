289f5a1bcfee96a1fb644b7f58602d3f170affdc
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2020-11-06 10:56:02 +0600

    typed.js added

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,6 @@ $('.our-skill').circleProgress({
   }).on('circle-animation-progress', function(event, progress) {
       var percentvalue=$(this).find('span').attr('data-value')
     $(this).find('span').html(Math.round(percentvalue * progress) + '<i>%</i>');
-    console.log(progress);
   });
  
 
@@ -12,4 +11,9 @@ $('.our-skill').circleProgress({
     animation: {
      duration: 300
     }
+  });
+
+  var typed = new Typed('.type', {
+    strings: ["A Programmer ^1000 ","A Observer ^1000 ","A Thinker ^1000 ","and Most Importantly^1000 ","A Human"],
+    typeSpeed: 90
   });
