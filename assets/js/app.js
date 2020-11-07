0bbd2c68bf48b2d1703ce941fb687af69d0b6e20
Erfan Ahmed Siam <erfan.siam98@gmail.com>
2020-11-07 11:57:28 +0600

    Particle JS added

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,10 @@ $('.our-skill').circleProgress({
     strings: ["A Programmer ^1000 ","An Observer ^1000 ","A Thinker ^1000 ","and Most Importantly^1000 ","A Human"],
     typeSpeed: 90
   });
+
+  //JS for particle
+
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
